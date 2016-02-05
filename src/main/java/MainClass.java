d6de921d28f8d0cba70fc4d27aec10ab82ff79c4
ybadache <yassine.badache@gmail.com>
2016-02-05 13:42:31 +0100

    Now adds member and date to card

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,47 +1,41 @@
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.trello4j.TrelloImpl;
+import org.trello4j.model.Member;
 
 public class MainClass {
 	final static String API_KEY = "1ae06ad5bee3715dbcfabb71342c54ed";
 
+	final static String BOARD_KEY = "56af6582ccde1bf9a1bfd85a";
+
 	final static String API_TOKEN = "ed4b95898da90220abc1c6e86e29e7b3d6b8a22bf5c759954a7cad3d678b5bfb";
 
 	public static void main(String[] args) throws IOException {
-		List<String> classList = new ArrayList<String>();
-		Map<String, String> idtolist = new HashMap<String, String>();
+		int i = 0;
+		Map<String, String> listKeyValueMap = new HashMap<String, String>();
 
 		TrelloImpl trello = new TrelloImpl(API_KEY, API_TOKEN);
 
+		// Creating the list, on top of the Trello board
+		listKeyValueMap.put("pos", "top");
+		org.trello4j.model.List todolist = trello.createList(BOARD_KEY, "TODO List", listKeyValueMap);
+
 		for (String line : Files.readAllLines(Paths.get("toDo2TrelloParsed.t2t"))) {
+			i++;
+			// name (0), task desc. (1), date (2), class (3), method (4)
 			String[] parts = line.split(";");
-			org.trello4j.model.List todolist;
 
-			if (!classList.contains(parts[3])) {
-				classList.add(parts[3]);
+			Map<String, String> cardKeyValueMap = new HashMap<String, String>();
+			cardKeyValueMap.put("due", "Due " + parts[2]);
+			cardKeyValueMap.put("desc", parts[1]);
+			Member dev = trello.getMember(parts[0].toLowerCase().replaceAll("\\s+", ""), (String[]) null);
+			cardKeyValueMap.put("idMembers", dev.getId());
 
-				Map<String, String> listKeyValueMap = new HashMap<String, String>();
-				listKeyValueMap.put("pos", "bottom");
-				todolist = trello.createList("56af6582ccde1bf9a1bfd85a", parts[3], listKeyValueMap);
-				idtolist.put(parts[3], todolist.getId());
-			}
-
-			else {
-				todolist = trello.getList(idtolist.get(parts[3]));
-			}
-
-			Map<String, String> listKeyValueMap = new HashMap<String, String>();
-
-			listKeyValueMap.put("desc", "\"" + parts[0] + "\" by " + parts[1] + " for " + parts[2]);
-
-			trello.createCard(todolist.getId(), parts[1], listKeyValueMap);
+			trello.createCard(todolist.getId(), parts[3] + ":" + parts[4], cardKeyValueMap);
 		}
 
 	}
