b34509daec5206ffe223d2d4836e7798026a2347
Ouamar Sais <ouamar.sais@gmail.com>
2016-02-06 16:06:17 +0100

    full name working

--- parse.sh
+++ parse.sh
@@ -3,12 +3,12 @@ IFS=$'\n'
 
 for line in $(cat $1)
 do
-	classname=$(echo $line | awk -F"in " '{print $2}' | cut -d ' ' -f 1)
-	method=$(echo $line | awk -F"in " '{print $2}' | cut -d ' ' -f 2)
-	name=$(echo $line | cut -d " " -f 4)
+	name="$(echo $line | cut -d " " -f 4) $(echo $line | cut -d " " -f 5)"
 	task=$(echo $line | cut -d "\"" -f 2)
 	date=$(echo $line | awk -F"for " '{print $2}' | cut -d ' ' -f 1)
-
+	classname=$(echo $line | awk -F"in " '{print $2}' | cut -d ' ' -f 1)
+	method=$(echo $line | awk -F"in " '{print $2}' | cut -d ' ' -f 2)
+	
 	echo "$name;$task;$date;$classname;$method" >> toDo2TrelloParsed.t2t
 	echo $name has to $task \for $date into $classname $method
 done
