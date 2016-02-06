fa21a5222fa4f63f131c5d4f571802e2cdafb128
Ouamar Sais <ouamar.sais@gmail.com>
2016-02-06 15:49:10 +0100

    add get name of method

--- getToDo.sh
+++ getToDo.sh
@@ -11,17 +11,20 @@ rm toDo2Trello.t2t
 
 files=$(find $1 -name '*.java')
 #regex='^public[[:space:]]+(abstract[[:space:]]+)?(enum|class|interface)[[:space:]]+[aA-zZ]+[[:space:]]*(extends[[:space:]]+[aA-zZ]+[[:space:]]*)?(implements[[:space:]]+[aA-zZ]+[[:space:]]*)?{'
+regex='(public|private|protected)[[:space:]]+(void|[aA-zZ]+)[[:space:]]+[[:alnum:]]+[[:space:]]*\('
 IFS=$'\n'
 
 for file in $files
 do	
     classname=$(echo $(basename $file) | cut -d '.' -f 1)
-
-	for line in $(grep "// TODO" $file)
+	for line in $(tail -r $file | grep -n "// TODO")
 	do
+		content=$(echo $line | cut -d ':' -f 2)
+		nbLine=$(echo $line | cut -d ':' -f 1)
+		method=$(tail -r $file | tail -n +$nbLine | grep -m 1 -E $regex | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f 3 | cut -d '(' -f 1)
 		if [[ $line != *"Auto-generated"* ]]
 		then
-			echo `echo $line | sed -e 's/^[ \t]*//' | sed -e 's/^[ 	]*//'` in $classname >> toDo2Trello.t2t
+			echo `echo $content | sed -e 's/^[[:space:]]*//' ` in $classname $method >> toDo2Trello.t2t
 		fi
 	done
 done
--- parse.sh
+++ parse.sh
@@ -4,12 +4,13 @@ IFS=$'\n'
 for line in $(cat $1)
 do
 	classname=$(echo $line | awk -F"in " '{print $2}' | cut -d ' ' -f 1)
+	method=$(echo $line | awk -F"in " '{print $2}' | cut -d ' ' -f 2)
 	name=$(echo $line | cut -d " " -f 4)
 	task=$(echo $line | cut -d "\"" -f 2)
 	date=$(echo $line | awk -F"for " '{print $2}' | cut -d ' ' -f 1)
 
-	echo "$name;$task;$date;$classname" >> toDo2TrelloParsed.t2t
-	echo $name has to $task \for $date into $classname 
+	echo "$name;$task;$date;$classname;$method" >> toDo2TrelloParsed.t2t
+	echo $name has to $task \for $date into $classname $method
 done
 
 rm $1
